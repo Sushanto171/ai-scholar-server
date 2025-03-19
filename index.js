af2a3a737ed8bb029879d4a1112e83891859a606
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-03-20 00:07:48 +0600

    [Created]: server environment

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PROT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Ai - Scholar now running......");
+});
+
+app.listen(port, () => {
+  console.log(`AI-Scholar Server running on port: ${port}`);
+});

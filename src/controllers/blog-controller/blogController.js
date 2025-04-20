0d368b6f59be44147e9bad89597925fd2cbb2595
Imranahmed935 <imrantahir9918@gmail.com>
2025-04-20 22:14:54 +0600

    create a modal for update blog

--- src/controllers/blog-controller/blogController.js
+++ src/controllers/blog-controller/blogController.js
@@ -61,10 +61,56 @@ const deleteBlog = async (req, res, next) => {
   }
 };
 
+const getDataForUpdate = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log("Getting blog with ID:", id);
+
+    const blogInfo = await Blog.findById(id);
+
+    if (!blogInfo) {
+      return res.status(404).json({
+        success: false,
+        message: "Blog not found",
+      });
+    }
+
+    sendResponse(res, 200, true, "Get a blog successfully", blogInfo);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// PATCH: /blogs/blog/:id
+const updatedBlogById = async(req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    const updatedBlog = await Blog.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Blog updated successfully",
+      data: updatedBlog,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+    next(error)
+  }
+};
+
+
+
 module.exports = {
   createBlog,
   getAllBlogs,
   getSingleBlog,
   AllBlogs,
-  deleteBlog
+  deleteBlog,
+  getDataForUpdate,
+  updatedBlogById
 };
